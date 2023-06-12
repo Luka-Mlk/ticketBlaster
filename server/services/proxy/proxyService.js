768da944a1e6e7ed775929d6e563e04f7d2b087c
Luka-Mlk <luka.milik1@gmail.com>
2023-06-13 00:43:36 +0200

    added proxy service

---
+++ server/services/proxy/proxyService.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const proxy = require("express-http-proxy");
+const cors = require("cors");
+
+const config = require("../../pkg/config/config");
+
+const api = express();
+
+api.use(cors());
+
+const storageProxy = proxy("http://localhost:4997", {
+  proxyReqPathResolver: (req) => {
+    return `/api/storage${req.url}`;
+  },
+});
+
+const usersProxy = proxy("http://localhost:4998", {
+  proxyReqPathResolver: (req) => {
+    return `/api/user${req.url}`;
+  },
+});
+
+const eventsProxy = proxy("http://localhost:4999", {
+  proxyReqPathResolver: (req) => {
+    return `/api/event${req.url}`;
+  },
+});
+
+api.use("/api/storage", storageProxy);
+api.use("/api/user", usersProxy);
+api.use("/api/event", eventsProxy);
+
+api.listen(config.get("services").proxy.port, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(
+    `Service [proxy] has successfully started on port ${
+      config.get("services").proxy.port
+    }`
+  );
+});
