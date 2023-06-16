f9013eb3e40e1dae5bf294234ff0e817c9c86e0a
Luka-Mlk <luka.milik1@gmail.com>
2023-06-16 03:02:43 +0200

    Made changes to creation authorities, added getByCategory and getByContent methods

--- server/services/events/handlers/eventsHandler.js
+++ server/services/events/handlers/eventsHandler.js
@@ -2,8 +2,11 @@ const event = require("../../../pkg/models/event/event.js");
 
 const createEvent = async (req, res) => {
   try {
+    console.log(req.auth);
     if (req.auth.admin) {
+      // if user is admin allow event creation
       const newEvent = await event.create({
+        // body must have only theese fields
         eventName: req.body.eventName,
         category: req.body.category,
         date: req.body.date,
@@ -18,11 +21,15 @@ const createEvent = async (req, res) => {
         event: newEvent,
       });
     }
-    // return res.status(400).send("Incorrect privileges");
-    return res.status(400).json({
-      status: "failed",
-      error: "Incorrect privileges",
-    });
+    if (typeof req.auth == "undefined" || !req.auth.admin) {
+      // if there is no token or token is false return bad request
+      // return res.status(400).send("Incorrect privileges");
+      return res.status(400).json({
+        // if user !admin refuse creation request
+        status: "failed",
+        error: "Incorrect privileges",
+      });
+    }
   } catch (err) {
     console.log(err);
     // return res.status(500).send("Internal server error");
@@ -69,9 +76,42 @@ const getOneEvent = async (req, res) => {
   }
 };
 
+const getEventByContent = async (req, res) => {
+  try {
+    const eventsByContent = await event.getByDetails(req.params.keyword); // returns array of events containing keyword
+    return res.status(200).json({
+      status: "success",
+      eventsByContent,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      status: "failed",
+      error: "Internal server error",
+    });
+  }
+};
+
+const getEventByCategory = async (req, res) => {
+  try {
+    const eventsByCategory = await event.getByCategory(req.params.category);
+    return res.status(200).json({
+      status: "success",
+      eventsByCategory,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      status: "failed",
+      error: "Internal server error",
+    });
+  }
+};
+
 const updateEvent = async (req, res) => {
   try {
     if (req.auth.admin) {
+      // if user is admin allow update
       const updatedEvent = event.update(req.params.id, req.body);
       // res.status(204).send(updatedEvent);
       return res.status(204).json({
@@ -81,6 +121,7 @@ const updateEvent = async (req, res) => {
     }
     // return res.status(400).send("Incorrect privileges");
     return res.status(400).json({
+      // if user !admin refuse update request
       status: "failed",
       error: "Incorrect privileges",
     });
@@ -97,6 +138,7 @@ const updateEvent = async (req, res) => {
 const removeEvent = async (req, res) => {
   try {
     if (req.auth.admin) {
+      // if user is admin allow deletion
       const removedEvent = event.remove(req.params.id);
       // return res.status(204).send(removedEvent);
       return res.status(204).json({
@@ -106,6 +148,7 @@ const removeEvent = async (req, res) => {
     }
     // res.status(400).send("Incorrect privileges");
     return res.status(400).json({
+      // if user is not admin refuse delete request
       status: "failed",
       error: "Incorrect privileges",
     });
@@ -123,6 +166,8 @@ module.exports = {
   createEvent,
   getAllEvents,
   getOneEvent,
+  getEventByContent,
+  getEventByCategory,
   updateEvent,
   removeEvent,
 };
