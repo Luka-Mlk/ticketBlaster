e49e73a913046ac72ca2c827475c148cf4546e72
Luka-Mlk <luka.milik1@gmail.com>
2023-07-16 01:27:01 +0200

    minor fixes around aesthetics

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../../components/header/Header";
-import LoginForm from "../../components/loginForm/loginFrom";
-import "../../assets/loginPage/loginPage.css";
+import LoginForm from "../../components/loginForm/LoginFrom";
 import Footer from "../../components/footer/Footer";
 
+import "../../assets/loginPage/loginPage.css";
 function Login() {
   return (
     <div className="login--page">
