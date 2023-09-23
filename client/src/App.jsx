b27057d90e04b99422e3840e61755eecfa72c669
Luka-Mlk <luka.milik1@gmail.com>
2023-09-23 19:37:38 +0200

    removed: unnused code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
 import Routing from "./routes/Routing";
 
 function App() {
